36754ef2a0876c45d8369228ac27a9050932e9a3
u0452046 <39178675+u0452046@users.noreply.github.com>
2018-05-16 23:45:33 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -2,17 +2,17 @@
 
 int main(void)
 {
-	int integer1;
+	int integer1; //設定變數 
 	int integer2;
 	int sum;
 	
 	printf("Enter first integer\n");
-	scanf("%d",&integer1);
+	scanf("%d",&integer1); //輸入第一個變數 
 	
 	printf("Enter second integer\n");
-	scanf("%d",&integer2);
+	scanf("%d",&integer2); //輸入第二個變數 
 	
-	sum=integer1+integer2;
+	sum=integer1+integer2; //將兩變數相加 
 	
-	printf("sum is %d\n",sum);
+	printf("sum is %d\n",sum); //輸出變數和 
 }
