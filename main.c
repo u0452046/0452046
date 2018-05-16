aae2d492d238e84b7ddc0b81def44ff9f23707d2
u0452046 <39178675+u0452046@users.noreply.github.com>
2018-05-16 23:43:11 +0800

    Add files via upload

--- main.c
+++ main.c
@@ -2,5 +2,17 @@
 
 int main(void)
 {
-	printf("Hello Welcome To C");
+	int integer1;
+	int integer2;
+	int sum;
+	
+	printf("Enter first integer\n");
+	scanf("%d",&integer1);
+	
+	printf("Enter second integer\n");
+	scanf("%d",&integer2);
+	
+	sum=integer1+integer2;
+	
+	printf("sum is %d\n",sum);
 }
